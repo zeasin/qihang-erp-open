5ff4e10822b0b78e0bc40e328714be6b09e84a52
启航 <280645618@qq.com>
2026-07-05 11:00:28 +0800

    修复：平台拉取日志接口404

--- erp-api/src/main/java/cn/qihangerp/erp/controller/oms/ShopPullLogsController.java
+++ erp-api/src/main/java/cn/qihangerp/erp/controller/oms/ShopPullLogsController.java
@@ -1,4 +1,4 @@
-package cn.qihangerp.erp.controller.erp;
+package cn.qihangerp.erp.controller.oms;
 
 import cn.qihangerp.common.PageQuery;
 import cn.qihangerp.common.TableDataInfo;
@@ -10,16 +10,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 店铺拉取日志 Controller
+ * 对应前端 /api/oms-api/shop/pull_logs_list
+ */
 @AllArgsConstructor
 @RestController
-@RequestMapping("/api/erp-api/shop")
+@RequestMapping("/api/oms-api/shop")
 public class ShopPullLogsController extends BaseController {
     private final OShopPullLogsService pullLogsService;
 
     @GetMapping("/pull_logs_list")
-    public TableDataInfo list(OShopPullLogs logs, PageQuery pageQuery)
-    {
-        var pageList = pullLogsService.queryPageList(logs,pageQuery);
+    public TableDataInfo list(OShopPullLogs logs, PageQuery pageQuery) {
+        var pageList = pullLogsService.queryPageList(logs, pageQuery);
         return getDataTable(pageList);
     }
 }
