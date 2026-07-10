cd2d0938d05300ef3f0a7242c86a56f1424275b0
richie <280645618@qq.com>
2026-07-10 21:21:57 +0800

    优化vue3

--- vue3/src/router/index.ts
+++ vue3/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 import Layout from '@/layout/index.vue'
 
@@ -59,7 +59,7 @@ export const constantRoutes: RouteRecordRaw[] = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: constantRoutes,
 })
 
